4effb94421bd921fa16a3d9d56da9af6f3981479
saWoW <swpants05@gmail.com>
2024-07-31 22:27:18 +0900

    Remove submodule and add FastMRI_challenge as a regular folder

---
+++ FastMRI_challenge/train.py
@@ -0,0 +1,81 @@
+import torch
+import argparse
+import shutil
+import os, sys
+from pathlib import Path
+
+if os.getcwd() + '/utils/model/' not in sys.path:
+    sys.path.insert(1, os.getcwd() + '/utils/model/')
+from utils.learning.train_part import train
+
+if os.getcwd() + '/utils/common/' not in sys.path:
+    sys.path.insert(1, os.getcwd() + '/utils/common/')
+from utils.common.utils import seed_fix
+
+
+def parse():
+    parser = argparse.ArgumentParser(description='Train Varnet on FastMRI challenge Images',
+                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-g', '--GPU-NUM', type=int, default=0, help='GPU number to allocate')
+    parser.add_argument('-b', '--batch-size', type=int, default=1, help='Batch size')
+    parser.add_argument('-e', '--num-epochs', type=int, default=1, help='Number of epochs')
+    parser.add_argument('-l', '--lr', type=float, default=1e-3, help='Learning rate')
+    parser.add_argument('-r', '--report-interval', type=int, default=500, help='Report interval')
+    parser.add_argument('-n', '--net-name', type=Path, default='test_varnet', help='Name of network')
+    parser.add_argument('-t', '--data-path-train', type=Path, default='/Data/train/', help='Directory of train data')
+    parser.add_argument('-v', '--data-path-val', type=Path, default='/Data/val/', help='Directory of validation data')
+    
+    parser.add_argument('--cascade', type=int, default=20, help='Number of cascades | Should be less than 12') ## important hyperparameter
+    parser.add_argument('--chans', type=int, default=18, help='Number of channels for cascade U-Net | 18 in original varnet') ## important hyperparameter
+    parser.add_argument('--sens_chans', type=int, default=8, help='Number of channels for sensitivity map U-Net | 8 in original varnet') ## important hyperparameter
+    parser.add_argument('--input-key', type=str, default='kspace', help='Name of input key')
+    parser.add_argument('--target-key', type=str, default='image_label', help='Name of target key')
+    parser.add_argument('--max-key', type=str, default='max', help='Name of max key in attributes')
+    parser.add_argument('--seed', type=int, default=430, help='Fix random seed')
+
+    # DataAugmentor 관련 argument 추가
+    parser.add_argument('--aug-on', default=False, action='store_true', help='Switch to turn data augmentation on')
+    parser.add_argument('--aug-schedule', type=str, default='exp', choices=['constant', 'ramp', 'exp'], help='Type of augmentation scheduling')
+    parser.add_argument('--aug-delay', type=int, default=0, help='Number of epochs without data augmentation at the start of training')
+    parser.add_argument('--aug-strength', type=float, default=0.3, help='Maximum augmentation strength')
+    parser.add_argument('--aug-exp-decay', type=float, default=5.0, help='Exponential decay coefficient for augmentation scheduling')
+    parser.add_argument('--aug-interpolation-order', type=int, default=1, help='Interpolation order used in augmentation, 1: bilinear, 3: bicubic')
+    parser.add_argument('--aug-upsample', default=False, action='store_true', help='Upsample before augmentation to avoid aliasing artifacts')
+    parser.add_argument('--aug-upsample-factor', type=int, default=2, help='Upsample factor before augmentation')
+    parser.add_argument('--aug-upsample-order', type=int, default=1, help='Order of upsampling filter before augmentation, 1: bilinear, 3: bicubic')
+
+    parser.add_argument('--aug-weight-translation', type=float, default=1.0, help='Weight of translation probability')
+    parser.add_argument('--aug-weight-rotation', type=float, default=1.0, help='Weight of arbitrary rotation probability')
+    parser.add_argument('--aug-weight-shearing', type=float, default=1.0, help='Weight of shearing probability')
+    parser.add_argument('--aug-weight-scaling', type=float, default=1.0, help='Weight of scaling probability')
+    parser.add_argument('--aug-weight-rot90', type=float, default=1.0, help='Weight of rotation by multiples of 90 degrees probability')
+    parser.add_argument('--aug-weight-fliph', type=float, default=1.0, help='Weight of horizontal flip probability')
+    parser.add_argument('--aug-weight-flipv', type=float, default=1.0, help='Weight of vertical flip probability')
+
+    parser.add_argument('--aug-max-translation-x', type=float, default=0.125, help='Maximum translation along the x axis as a fraction of image width')
+    parser.add_argument('--aug-max-translation-y', type=float, default=0.125, help='Maximum translation along the y axis as a fraction of image height')
+    parser.add_argument('--aug-max-rotation', type=float, default=180., help='Maximum rotation applied in either direction (degrees)')
+    parser.add_argument('--aug-max-shearing-x', type=float, default=15.0, help='Maximum shearing along the x axis (degrees)')
+    parser.add_argument('--aug-max-shearing-y', type=float, default=15.0, help='Maximum shearing along the y axis (degrees)')
+    parser.add_argument('--aug-max-scaling', type=float, default=0.25, help='Maximum scaling as a fraction of image dimensions')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = parse()
+    
+    # fix seed
+    if args.seed is not None:
+        seed_fix(args.seed)
+
+    args.gradient_accumulation_steps = 10
+    args.exp_dir = '../result' / args.net_name / 'checkpoints'
+    args.val_dir = '../result' / args.net_name / 'reconstructions_val'
+    args.main_dir = '../result' / args.net_name / __file__
+    args.val_loss_dir = '../result' / args.net_name
+
+    args.exp_dir.mkdir(parents=True, exist_ok=True)
+    args.val_dir.mkdir(parents=True, exist_ok=True)
+
+    train(args)
