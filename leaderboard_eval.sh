cb7cfdd6fded402c95117e482037e3b43c3789d2
sawo0150 <swpants05@gmail.com>
2024-08-19 11:28:48 +0900

    Update leaderboard_eval.sh

--- leaderboard_eval.sh
+++ leaderboard_eval.sh
@@ -1,3 +1,3 @@
 python3 leaderboard_eval.py \
   -lp '/home/Data/leaderboard' \
-  -yp '/root/FastMRI_sungsimdang/result/sungsimV2_promptMR/reconstructions_leaderboard'
+  -yp '/root/result/sungsimV2_promptMR/reconstructions_leaderboard'
